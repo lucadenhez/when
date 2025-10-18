28538f2063dc2b4f407260978baade7e93242138
Luca Denhez <lucadenhez@icloud.com>
2025-10-18 16:07:09 -0700

    work

---
+++ frontend/app/availability/page.jsx
@@ -0,0 +1,12 @@
+export default function Availability() {
+  const
+  
+  return (
+    <div className="flex flex-col m-10 items-center">
+      <p className="my-30">Import your favorite calendar to join this event</p>
+      <div className="flex flex-col items-center w-full gap-3">
+        
+      </div>
+    </div>
+  );
+}
