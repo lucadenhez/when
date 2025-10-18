fce6152dfd0fe17c582b93d8fb45e60a78261387
Luca Denhez <lucadenhez@icloud.com>
2025-10-18 14:46:19 -0700

    Auth add functionality

--- frontend/api/firebase/firebase.js
+++ frontend/api/firebase/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -19,7 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
---
+++ frontend/app/login/page.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { useState, useEffect } from "react";
+import { PhoneInput } from "../components/authentication/PhoneInput";
+import { setupRecaptcha, sendVerificationCode, verifyCodeAndSignIn } from "../../api/firebase/auth";
+
+export default function Login() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [verificationCode, setVerificationCode] = useState('');
+  const [confirmationResult, setConfirmationResult] = useState(null);
+
+  async function onSubmit(event) {
+    event.preventDefault()
+    setIsLoading(true)
+
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 3000)
+  }
+
+  useEffect(() => {
+    setupRecaptcha();
+  }, []);
+
+  const handleSendCode = async () => {
+    const result = await sendVerificationCode(phoneNumber);
+    setConfirmationResult(result);
+  };
+
+  const handleVerifyCode = async () => {
+    const verify = await verifyCodeAndSignIn(confirmationResult, verificationCode)
+  }
+
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center gap-5">
+      <p className="text-center font-semibold text-3xl">Welcome to When</p>
+      <form
+        onSubmit={onSubmit}
+        className="flex flex-col items-center gap-5"
+      >
+        <PhoneInput
+          defaultCountry="US"
+          placeholder="Phone Number"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <Button disabled={isLoading} onClick={() => handleSendCode() }>
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : <p>Send verification code</p>}
+
+        </Button>
+      </form>
+    </div>
+  );
+}
