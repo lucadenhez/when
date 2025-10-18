fd3bafa15df5a4e184d7e2ecf1e61b04677dec77
yaffenator <noamyaffe19@gmail.com>
2025-10-18 14:40:05 -0700

    random commit

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,25 @@
-from fastapi import FastAPI
-from googleapiclient.discovery import build
-from google.oauth2.credentials import Credentials
+import os
+import uvicorn
+import datetime
+from fastapi import FastAPI, Request, HTTPException, Depends, status
+from fastapi.responses import RedirectResponse, JSONResponse
+from starlette.middleware.sessions import SessionMiddleware
 from google_auth_oauthlib.flow import Flow
-from firebase_admin import firestore, db as realtimedb
-from firebase_admin import credentials, initialize_app, auth
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+
+CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
+CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
+REDIRECT_URI = os.environ.get('GOOGLE_REDIRECT_URI', 'http://localhost:8000/api/auth/google/callback')
+SCOPES = ['https://www.googleapis.com/auth/calendar.freebusy']
+FRONTEND_URL = 'http://localhost:3000'
 
 app = FastAPI()
+
+app.add_middleware(
+    allow_origins=[FRONTEND_URL],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
